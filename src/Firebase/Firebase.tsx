a20d103ff112c59e2cd029a8a213deea7257fe67
{n-rick2972} <{skyhigh.kbkr2972@gmail.com}>
2025-07-31 21:41:43 +0900

    🐛 fix firebase

--- src/Firebase/Firebase.tsx
+++ src/Firebase/Firebase.tsx
@@ -3,7 +3,6 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -19,4 +18,3 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
-const analytics = getAnalytics(app);
